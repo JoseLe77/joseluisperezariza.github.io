af3a1446b0f012a54a1fa0ed828e94f997cdd2ac
Josele <83826345+JoseLe77@users.noreply.github.com>
2024-04-29 10:31:37 +0200

    include CV
    Include data folder and add markdown  file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const mycv =document.querySelector('#mycv')
-const md = fetch("https://github.com/JoseLe77/CV/blob/main/Curriculum%20Jose%20Luis%20Perez%20Ariza.md").then((response) => response.text()).then(text=>{
+const md = fetch("../data/Curriculum Jose Luis Perez Ariza.md").then((response) => response.text()).then(text=>{
     const converter = new showdown.Converter()
     const html = converter.makeHtml(text)
     mycv.innerHTML = html
