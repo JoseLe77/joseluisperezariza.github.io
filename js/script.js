fb2a06eebd1d6253b40a3ccecd57d3ed0cbf18b2
Arimotla-Projects <joseluisperezariza@gmail.com>
2024-07-17 19:00:30 +0200

    Mail form fix

--- js/script.js
+++ js/script.js
@@ -32,11 +32,8 @@ const form = document.getElementById('myForm');
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // Evita el envío del formulario por defecto
 
-    const email = form.email.value;
-    const mensaje = form.mensaje.value;
-
     // Aquí puedes personalizar la dirección de correo electrónico y el cuerpo del mensaje
-    const mailtoLink = `mailto:joseluisperezariza@gmail.com?subject=Enviado desde Portfolio por: ${email}&body=Contacto desde portfolio: ${mensaje}`;
+    const mailtoLink = `mailto:joseluisperezariza@gmail.com`;
 
     // Crea un enlace y simúla un clic para abrir el cliente de correo
     const link = document.createElement('a');
